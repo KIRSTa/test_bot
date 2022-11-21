6056a4de985f4eaeea7f3c8a9d76bd67ea3d8741
KIRSTa <kirstbeats@gmail.com>
2022-11-21 20:09:13 +0300

    project init

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,42 @@
 import "dotenv/config"
 import { Telegraf } from 'telegraf'
 import {GoogleSpreadsheet} from 'google-spreadsheet'
+import { assert } from "console"
 
-
-
-// const tgToken = "5532864814:AAGbLT5uiWAF3f3O2mH47xWsRHsyEt4V2f0"
-const doc = new GoogleSpreadsheet("1wBrHB6meYcmRCv_yrCj605czwtyqwDdsvnAl8SYDiXA")
-const bot = new Telegraf(String(process.env.TG_TOKEN))
 // const bot = new Telegraf(tgToken)
-// bot.start((ctx) => ctx.reply('Welcome'));
-// bot.help((ctx) => ctx.reply('Send me a sticker'));
-// bot.on('sticker', (ctx) => ctx.reply('👍'));
-// bot.hears('hi', (ctx) => ctx.reply('Hey'));
-bot.launch();
+const bot = new Telegraf(String(process.env.TG_TOKEN))
+assert(bot !=null,'No TG_TOKEN environment variable found')
 
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+const doc = new GoogleSpreadsheet("1wBrHB6meYcmRCv_yrCj605czwtyqwDdsvnAl8SYDiXA")
+
+const p2p_email = (String(process.env.P2P_MANGER_EMAIL))
+const p2p_key = (String(process.env.P2P_MANGER_KEY))
+
+assert(p2p_email !=null,'No P2P_MANAGE_EMAIL environment variable found')
+assert(p2p_key !=null,'No P2P_MANGER_KEY environment variable found')
+
+
+start()
+async function start () {
+    try{
+        await doc.useServiceAccountAuth({
+            client_email: p2p_email!, 
+            private_key: p2p_key!})
+    } catch(e){
+        console.error(e)
+    }
+    
+    
+    bot.start((ctx) => ctx.reply('Welcome'));
+    bot.on('text',async(ctx)=>{
+        console.info(ctx)
+    })
+    // bot.help((ctx) => ctx.reply('Send me a sticker'));
+    // bot.on('sticker', (ctx) => ctx.reply('👍'));
+    // bot.hears('hi', (ctx) => ctx.reply('Hey'));
+    bot.launch();
+    
+    // Enable graceful stop
+    process.once('SIGINT', () => bot.stop('SIGINT'));
+    process.once('SIGTERM', () => bot.stop('SIGTERM')); 
+}
