a130c1563a1709fc09c890222e71e5251f573622
KIRSTa <kirstbeats@gmail.com>
2022-12-04 15:04:18 +0300

    work

---
+++ database/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+const app= express();
+const port = process.env.PORT || 3000;
+import db from '../models/index.js';
+
+db.sequelize.sync ().then(()=>{
+    app.listen(port,()=>{
+        console.log(`App listening on port ${port}`);
+        
+    })
+
+})
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import "dotenv/config"
 import { Telegraf } from 'telegraf'
-import {GoogleSpreadsheet} from 'google-spreadsheet'
+import { google } from "googleapis"
 import { assert } from "console"
 
 // const bot = new Telegraf(tgToken)
 const bot = new Telegraf(String(process.env.TG_TOKEN))
 assert(bot !=null,'No TG_TOKEN environment variable found')
 
-const doc = new GoogleSpreadsheet("1wBrHB6meYcmRCv_yrCj605czwtyqwDdsvnAl8SYDiXA")
+
 
 const p2p_email = (String(process.env.P2P_MANGER_EMAIL))
 const p2p_key = (String(process.env.P2P_MANGER_KEY))
@@ -16,27 +16,35 @@ assert(p2p_email !=null,'No P2P_MANAGE_EMAIL environment variable found')
 assert(p2p_key !=null,'No P2P_MANGER_KEY environment variable found')
 
 
-start()
-async function start () {
-    try{
-        await doc.useServiceAccountAuth({
-            client_email: p2p_email!, 
-            private_key: p2p_key!})
-    } catch(e){
-        console.error(e)
-    }
-    
-    
-    bot.start((ctx) => ctx.reply('Welcome'));
-    bot.on('text',async(ctx)=>{
-        console.info(ctx)
+;(async () => {
+    const auth = new google.auth.JWT({
+      email: p2p_email,
+      key: p2p_key,
+      scopes: ["https://accounts.google.com/o/oauth2/auth"]
     })
+    const sheet = google.sheets("v4")
+    await sheet.spreadsheets.values.append({
+      spreadsheetId:(String(process.env.SHEET_ID)),
+      auth: auth,
+      range: "Sheet1",
+      valueInputOption: "RAW",
+      requestBody: {
+        values: [["hello", "world"]]
+      }
+    })
+  })()
+    
+    
+    // bot.start((ctx) => ctx.reply('Welcome'));
+    // bot.on('text',async(ctx)=>{
+    //     console.info(ctx)
+    // })
     // bot.help((ctx) => ctx.reply('Send me a sticker'));
     // bot.on('sticker', (ctx) => ctx.reply('👍'));
     // bot.hears('hi', (ctx) => ctx.reply('Hey'));
-    bot.launch();
+    // bot.launch();
     
-    // Enable graceful stop
-    process.once('SIGINT', () => bot.stop('SIGINT'));
-    process.once('SIGTERM', () => bot.stop('SIGTERM')); 
-}
+    // // Enable graceful stop
+    // process.once('SIGINT', () => bot.stop('SIGINT'));
+    // process.once('SIGTERM', () => bot.stop('SIGTERM')); 
+
